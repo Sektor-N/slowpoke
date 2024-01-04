f342787d9ca416eaa0d9e8674c7b6bff5fe1a60f
Tobias Viertel <tviertel@sektorn.de>
2024-01-04 16:36:07 +0100

    Revert "feat: increment workers on timeout and decrement on completed"
    This reverts commit 1c6d0d895d1aa243e3ca4d8af4fc853c67516b48.

--- lib/slowpoke.rb
+++ lib/slowpoke.rb
@@ -35,15 +35,15 @@ module Slowpoke
   end
 end
 
+# remove noisy logger
+Rack::Timeout.unregister_state_change_observer(:logger)
+
+# process protection and notifications
 Rack::Timeout.register_state_change_observer(:slowpoke) do |env|
-  case env[Rack::Timeout::ENV_INFO_KEY].state
-  when :timed_out
+  if env[Rack::Timeout::ENV_INFO_KEY].state == :timed_out
     env[Slowpoke::ENV_KEY] = true
-    Process.kill("TTIN", Process.ppid)
-  when :completed
-    if env[Slowpoke::ENV_KEY]
-      Process.kill("QUIT", Process.pid)
-      Process.kill("TTOU", Process.ppid)
-    end
+
+    # TODO better payload
+    ActiveSupport::Notifications.instrument("timeout.slowpoke", {})
   end
 end
